cae7f458b6ff27e91d39e9e737c15000a995bf3a
Yhuan shin <tejima.177642@sjdelmonte.sti.edu.ph>
2025-02-23 14:30:51 +0800

    Update admin authentication model to use User model instead of Admin model

--- web-based-app/config/auth.php
+++ web-based-app/config/auth.php
@@ -75,7 +75,7 @@ return [
 
         'admin' => [
             'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\Admin::class),
+            'model' => env('AUTH_MODEL', App\Models\User::class),
         ],
 
        
